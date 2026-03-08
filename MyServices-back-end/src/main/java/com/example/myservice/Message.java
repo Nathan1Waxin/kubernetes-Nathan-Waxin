6a8c8d23af4de8d60e24fc38888caa65f989fc35
Nathan1Waxin <nathan.waxin@etu.unilim.fr>
2026-03-08 15:49:13 +0100

    connect mysql

---
+++ MyServices-back-end/src/main/java/com/example/myservice/Message.java
@@ -0,0 +1,28 @@
+package com.example.myservice;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "messages")
+public class Message {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String content;
+    
+    // Constructeurs
+    public Message() {}
+    
+    public Message(String content) {
+        this.content = content;
+    }
+    
+    // Getters et Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    
+    public String getContent() { return content; }
+    public void setContent(String content) { this.content = content; }
+}
--- MyServices-back-end/src/main/java/com/example/myservice/MyWebService.java
+++ MyServices-back-end/src/main/java/com/example/myservice/MyWebService.java
@@ -1,14 +1,25 @@
 package com.example.myservice;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import jakarta.persistence.*;
 
 @RestController
 public class MyWebService {
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @RequestMapping(path = "/hello", method = RequestMethod.GET)
     public String hello(){
-        return " World !";
+        Message msg = new Message("World !");
+        entityManager.persist(msg);
+        
+        return " World ! (saved in MySQL with ID: " + msg.getId() + ")";
+    }
+    
+    @GetMapping("/messages")
+    public String getMessages() {
+        long count = (Long) entityManager.createQuery("SELECT COUNT(m) FROM Message m").getSingleResult();
+        return "Total messages in database: " + count;
     }
 }
